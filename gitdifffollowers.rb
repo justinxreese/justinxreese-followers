6f2ffa40c62ef1578b6e85f4ca42329cae0b8229
Justin Reese <justin.x.reese@gmail.com>
2011-08-05 00:23:34 -0400

    Initial commit

---
+++ gitdifffollowers.rb
@@ -0,0 +1,17 @@
+require "yaml"
+require "twitter"
+
+key_file = YAML.load_file('config.yml')
+
+Twitter.configure do |config|
+  config.consumer_key = key_file['consumer_key']
+  config.consumer_secret = key_file['consumer_secret']
+  config.oauth_token = key_file['oauth_token']
+  config.oauth_token_secret = key_file['oauth_token_secret']
+end
+
+follower_file = File.new('followers','w')
+followers = Twitter.friends.users.sort{|x,y| x.id <=> y.id}.collect{|x| x.screen_name}
+followers.each do |f|
+  follower_file.puts f
+end
